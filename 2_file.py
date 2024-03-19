5ef87b3f71f9d8d09d881dd66b69932fa550fbf3
MuminAziza <161857292+MuminAziza@users.noreply.github.com>
2024-03-19 14:41:22 +0300

    Add files via upload
    Lesson_1
    Lesson_2
    Lesson_3

---
+++ 2_file.py
@@ -0,0 +1,7 @@
+# first code for python
+print  ("Hello dogs! 1")
+print  ("Hello dogs! 2")
+print  ("Hello dogs! 3")
+
+#etot kod rabotayet kak nado!
+# Avtor Aziza
---
+++ 3_file.py
@@ -0,0 +1,8 @@
+# second code for python
+print("Hello dogs 1!")
+print("Hello dogs 2!")
+print("Hello dogs 3!")
+print("Hello dogs 4!")
+
+# etot kod rabotayet spravno
+# Avtor razrabotchik # 2
